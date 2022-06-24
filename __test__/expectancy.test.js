2811e76663cc59d2efcc7a2f89fa98bc440ee526
Garrett Hays <GarrettlHays@gmail.com>
2022-06-24 14:00:27 -0700

    red phase - calculate years left in mercury years

--- __test__/expectancy.test.js
+++ __test__/expectancy.test.js
@@ -1,7 +1,7 @@
-import {LifeExpectancy} from '../src/js/expectancy.js';
+import {lifeExpectancy, mercuryExpectancy, mercuryExpected} from '../src/js/expectancy.js';
 import GalacticAge from '../src/js/galactic.js';
 
-describe('LifeExpectancy', () => {
+describe('lifeExpectancy', () => {
   let age;
 
   beforeEach (() => {
@@ -9,12 +9,26 @@ describe('LifeExpectancy', () => {
   });
 
   test('should calculate years left between life expectancy and user age on Earth', () => {
-    let timeLeft = LifeExpectancy(33, 79);
+    let timeLeft = lifeExpectancy(33, 79);
     expect(timeLeft).toEqual(46);    
   });
 
   test('should output years over life expectancy if user age is greater than expectancy', () => {
-    let timeLeft = LifeExpectancy(89, 79);
+    let timeLeft = lifeExpectancy(89, 79);
     expect(timeLeft).toEqual(10);
+  });
+});
+
+describe('mercuryExpectancy', () => {
+  let age;
+
+  beforeEach (() => {
+    age = new GalacticAge(33,79);
+  });
+  test('should calculate years left in Mercury years', () => {
+    age.earthDays();
+    let mercuryAge = age.convertMercury();
+    let mercuryTimeLeft = mercuryExpectancy(mercuryAge, 328);
+    expect(mercuryTimeLeft).toEqual(191);
   });
 });
