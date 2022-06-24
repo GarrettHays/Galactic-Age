19c301804bfa39bb2eb970d33e4878c5eec36477
Garrett Hays <GarrettlHays@gmail.com>
2022-06-24 09:46:05 -0700

    red phase - test 2 - receive life expectancy

--- __test__/galactic.test.js
+++ __test__/galactic.test.js
@@ -10,4 +10,8 @@ describe('galacticAge', () => {
   test('should take user age input then save to object', () => {
     expect(age.age).toEqual(33);
   });
+
+  test('should take user life expectancy then save to object', () => {
+    expect(age.expected).toEqual(79);
+  });
 });
