e693072b1e4035671d28b5fb0ce9f5a584d70bd3
Garrett Hays <GarrettlHays@gmail.com>
2022-06-24 13:42:00 -0700

    green phase - outputs years over life expectancy for user on life overtime

--- src/js/expectancy.js
+++ src/js/expectancy.js
@@ -1,4 +1,8 @@
 export const LifeExpectancy = (age, expected) => {
   let timeLeft = (expected - age);
+  if (timeLeft < 0) {
+    let overTime = Math.abs(timeLeft);
+    return overTime;
+  }
   return timeLeft;
 };
