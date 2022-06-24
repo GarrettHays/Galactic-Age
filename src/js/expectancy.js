d951fa7fc8ea2566469b334bb57d06c0d9ea57a2
Garrett Hays <GarrettlHays@gmail.com>
2022-06-24 14:01:26 -0700

    green phase - return years left in mercury years

--- src/js/expectancy.js
+++ src/js/expectancy.js
@@ -1,8 +1,13 @@
-export const LifeExpectancy = (age, expected) => {
+export const lifeExpectancy = (age, expected) => {
   let timeLeft = (expected - age);
   if (timeLeft < 0) {
     let overTime = Math.abs(timeLeft);
     return overTime;
   }
   return timeLeft;
+};
+
+export const mercuryExpectancy = (mercuryAge, expected) => {
+  let mercuryTimeLeft = (expected - mercuryAge);
+  return mercuryTimeLeft;
 };
