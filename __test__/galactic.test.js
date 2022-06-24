bd660fabaf12f8154370190c9b06cac87c26859a
Garrett Hays <GarrettlHays@gmail.com>
2022-06-24 10:32:59 -0700

    red phase - test 3 and 4 - updates to test 3, establishes test 4 to calculate user age in days

--- __test__/galactic.test.js
+++ __test__/galactic.test.js
@@ -24,8 +24,13 @@ describe('earthDays', () => {
   });
 
   test('should save user age input in days as variable', () => {
-    let totalDaysAge = age.earthDays();
-    let totalYearsAge = totalDaysAge / 365;
+    let totalLifeDays = age.earthDays();
+    let totalYearsAge = totalLifeDays / 365;
     expect(totalYearsAge).toEqual(33);
+  });
+
+  test('should multiply user age in days by 365 to set age in total earth days as variable', () => {
+    let totalLifeDays = age.earthDays();
+    expect(totalLifeDays).toEqual(12045);
   });
 });
