b2b6308178bc31cf8641417f40a91114715c9ba0
Garrett Hays <GarrettlHays@gmail.com>
2022-06-24 10:35:46 -0700

    green phase - test 4 - user input multiplied by 365 and saved as totalLifeDays variable

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -1,15 +1,14 @@
 export default class GalacticAge {
   constructor(age, expected) {
     this.age = age;
-    this.years = 0;
     this.days = 0;
     this.expected = expected;
   }
 
   earthDays(){
     let userAge = this.age;
-    let totalDaysAge = userAge * 365;
-    this.days = totalDaysAge;
-    return totalDaysAge;
+    let totalLifeDays = userAge * 365;
+    this.days = totalLifeDays;
+    return totalLifeDays;
   }
 }
