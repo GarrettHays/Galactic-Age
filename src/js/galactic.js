ef8fcca24578ad04b4fddd2154a5744763c28092
Garrett Hays <GarrettlHays@gmail.com>
2022-06-24 10:17:05 -0700

    green phase - test 3 - user input saved as days in variable

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -2,6 +2,14 @@ export default class GalacticAge {
   constructor(age, expected) {
     this.age = age;
     this.years = 0;
+    this.days = 0;
     this.expected = expected;
+  }
+
+  earthDays(){
+    let userAge = this.age;
+    let totalDaysAge = userAge * 365;
+    this.days = totalDaysAge;
+    return totalDaysAge;
   }
 }
